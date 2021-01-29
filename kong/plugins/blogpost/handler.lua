942bf0171dec927cf03bb404f85156bbd5f66bf4
Thijs Schreijer <thijs@thijsschreijer.nl>
2021-01-29 16:04:20 +0100

    update plugin handler to post Kong 1.0 structure

--- kong/plugins/blogpost/handler.lua
+++ kong/plugins/blogpost/handler.lua
@@ -1,18 +1,12 @@
-local BasePlugin = require "kong.plugins.base_plugin"
 local access = require "kong.plugins.custom-auth.access"
 
-local TokenHandler = BasePlugin:extend()
-
-function TokenHandler:new()
-    TokenHandler.super.new(self, "custom-auth")
-end
+local TokenHandler = {
+    VERSION = "1.0",
+    PRIORITY = 1000,
+}
 
 function TokenHandler:access(conf)
-    TokenHandler.super.access(self)
     access.run(conf)
 end
-
-TokenHandler.VERSION = "1.0"
-TokenHandler.PRIORITY = 1000
 
 return TokenHandler
