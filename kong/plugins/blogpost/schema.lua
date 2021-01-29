d5fc6254c4a06759f46435df0c3ba64a578b2e93
Thijs Schreijer <thijs@thijsschreijer.nl>
2021-01-29 15:58:07 +0100

    fix schema: valid header name, do not allow consumer

--- kong/plugins/blogpost/schema.lua
+++ kong/plugins/blogpost/schema.lua
@@ -5,12 +5,13 @@ return {
   name = "custom-auth",
   fields = {
     { protocols = typedefs.protocols_http },
+    { consumer = typedefs.no_consumer },
     { config = {
         type = "record",
         fields = {
           { introspection_endpoint = typedefs.url({ required = true }) },
           { authorization_endpoint = typedefs.url({ required = true }) },
-          { token_header = { type = "string", default = "Authorization", required = true }, }
+          { token_header = typedefs.header_name { default = "Authorization", required = true }, }
     }, }, },
   },
 }
