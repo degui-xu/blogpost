b9bbda90ac142e95efc8ee8aa0f777bdc3052e62
Degui Xu <53922078+degui-xu@users.noreply.github.com>
2021-01-31 18:03:45 +0800

    Update handler.lua
    Changing handler.lua file back to pongo template

--- kong/plugins/blogpost/handler.lua
+++ kong/plugins/blogpost/handler.lua
@@ -1,68 +1,103 @@
-local http = require "resty.http"
-local utils = require "kong.tools.utils"
+-- If you're not sure your plugin is executing, uncomment the line below and restart Kong
+-- then it will throw an error which indicates the plugin is being loaded at least.
 
-local TokenHandler = {
-    VERSION = "1.0",
-    PRIORITY = 1000,
+--assert(ngx.get_phase() == "timer", "The world is coming to an end!")
+
+---------------------------------------------------------------------------------------------
+-- In the code below, just remove the opening brackets; `[[` to enable a specific handler
+--
+-- The handlers are based on the OpenResty handlers, see the OpenResty docs for details
+-- on when exactly they are invoked and what limitations each handler has.
+---------------------------------------------------------------------------------------------
+
+
+
+local plugin = {
+  PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
+  VERSION = "0.1",
 }
 
 
-local function introspect_access_token(conf, access_token, customer_id)
-  local httpc = http:new()
-  -- step 1: validate the token
-  local res, err = httpc:request_uri(conf.introspection_endpoint, {
-      method = "POST",
-      ssl_verify = false,
-      headers = {
-          ["Content-Type"] = "application/x-www-form-urlencoded",
-          ["Authorization"] = "Bearer " .. access_token }
-  })
 
-  if not res then
-      kong.log.err("failed to call introspection endpoint: ",err)
-      return kong.response.exit(500)
-  end
-  if res.status ~= 200 then
-      kong.log.err("introspection endpoint responded with status: ",res.status)
-      return kong.response.exit(500)
-  end
-
-  -- step 2: validate the customer access rights
-  local res, _ = httpc:request_uri(conf.authorization_endpoint, {
-      method = "POST",
-      ssl_verify = false,
-      body = '{ "custId":"' .. customer_id .. '"}',
-      headers = { ["Content-Type"] = "application/json",
-          ["Authorization"] = "Bearer " .. access_token }
-  })
-
-  if not res then
-    kong.log.err("failed to call authorization endpoint: ",err)
-    return kong.response.exit(500)
-  end
-  if res.status ~= 200 then
-      kong.log.err("authorization endpoint responded with status: ",res.status)
-      return kong.response.exit(500)
-  end
-
-  return true -- all is well
-end
-
-function TokenHandler:access(conf)
-  local access_token = ngx.req.get_headers()[conf.token_header]
-  if not access_token then
-      kong.response.exit(401)  --unauthorized
-  end
-  -- replace Bearer prefix
-  access_token = access_token:sub(8,-1) -- drop "Bearer "
-  local request_path = ngx.var.request_uri
-  local values = utils.split(request_path, "/")
-  local customer_id = values[3]
-
-  introspect_access_token(conf, access_token, customer_id)
-
-  kong.service.clear_header(conf.token_header)
-end
+-- do initialization here, any module level code runs in the 'init_by_lua_block',
+-- before worker processes are forked. So anything you add here will run once,
+-- but be available in all workers.
 
 
-return TokenHandler
+
+-- handles more initialization, but AFTER the worker process has been forked/created.
+-- It runs in the 'init_worker_by_lua_block'
+function plugin:init_worker()
+
+  -- your custom code here
+  kong.log.debug("saying hi from the 'init_worker' handler")
+
+end --]]
+
+
+
+--[[ runs in the 'ssl_certificate_by_lua_block'
+-- IMPORTANT: during the `certificate` phase neither `route`, `service`, nor `consumer`
+-- will have been identified, hence this handler will only be executed if the plugin is
+-- configured as a global plugin!
+function plugin:certificate(plugin_conf)
+
+  -- your custom code here
+  kong.log.debug("saying hi from the 'certificate' handler")
+
+end --]]
+
+
+
+--[[ runs in the 'rewrite_by_lua_block'
+-- IMPORTANT: during the `rewrite` phase neither `route`, `service`, nor `consumer`
+-- will have been identified, hence this handler will only be executed if the plugin is
+-- configured as a global plugin!
+function plugin:rewrite(plugin_conf)
+
+  -- your custom code here
+  kong.log.debug("saying hi from the 'rewrite' handler")
+
+end --]]
+
+
+local access = require "kong.plugins.myplugin.access"
+-- runs in the 'access_by_lua_block'
+function plugin:access(plugin_conf)
+
+  -- your custom code here
+  kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
+  --ngx.req.set_header(plugin_conf.request_header, "this is on a request")
+  access.run(plugin_conf)
+end --]]
+
+
+-- runs in the 'header_filter_by_lua_block'
+function plugin:header_filter(plugin_conf)
+
+  -- your custom code here, for example;
+  ngx.header[plugin_conf.response_header] = "this is on the response"
+
+end --]]
+
+
+--[[ runs in the 'body_filter_by_lua_block'
+function plugin:body_filter(plugin_conf)
+
+  -- your custom code here
+  kong.log.debug("saying hi from the 'body_filter' handler")
+
+end --]]
+
+
+--[[ runs in the 'log_by_lua_block'
+function plugin:log(plugin_conf)
+
+  -- your custom code here
+  kong.log.debug("saying hi from the 'log' handler")
+
+end --]]
+
+
+-- return our plugin object
+return plugin
